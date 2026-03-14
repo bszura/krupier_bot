5100da65f64a9338e574c52a2fb3695878cffddd
bszura <bartosz1szura@gmail.com>
2026-03-14 11:20:48 +0100

    initial commit

--- files/Program.cs
+++ files/Program.cs
@@ -4,6 +4,27 @@ using Discord.Interactions;
 using Microsoft.Extensions.DependencyInjection;
 using DiscordBot.Services;
 
+// Minimalny serwer HTTP dla Render + Uptime Robot
+var http = new Thread(() =>
+{
+    var listener = new System.Net.HttpListener();
+    listener.Prefixes.Add($"http://*:{Environment.GetEnvironmentVariable("PORT") ?? "8080"}/");
+    listener.Start();
+    Console.WriteLine("✅ HTTP listener gotowy.");
+    while (true)
+    {
+        var ctx = listener.GetContext();
+        var resp = ctx.Response;
+        var msg = System.Text.Encoding.UTF8.GetBytes("OK");
+        resp.ContentLength64 = msg.Length;
+        resp.OutputStream.Write(msg, 0, msg.Length);
+        resp.OutputStream.Close();
+    }
+});
+http.IsBackground = true;
+http.Start();
+
+// Discord bot
 var config = new DiscordSocketConfig
 {
     GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
